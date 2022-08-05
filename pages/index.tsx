150cea7fb9165078a9f31277da1ee2ece1ca9be5
unknown <iyinoluwa05@gmail.com>
2022-08-05 16:01:11 +0100

    frey

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,16 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+// import Image from 'next/image'
+import { sanityClient, urlFor } from "../sanity"
+import { Post } from "../typings"
 
-const Home: NextPage = () => {
+interface Props {
+  posts: [Post];
+}
+
+// const Home: NextPage = (props: Props) => {
+export default function Home({ posts }: Props) {
+  console.log(posts)
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
@@ -11,9 +19,31 @@ const Home: NextPage = () => {
       </Head>
       <h1 className="">Hello World</h1>
       <p>replacer</p>
-      </div>
+    </div>
 
   )
 }
 
-export default Home
+// export default Home
+
+export const getServersideProps = async () => {
+  const query = `*[_type == "post"] {
+    _id,
+    title,
+    author-> {
+      name,
+      image
+    },
+    description,
+    mainImage,
+    slug
+  }`;
+
+  const posts = await sanityClient.fetch(query);
+
+  return {
+    props: {
+      posts,
+    }
+  }
+}
--- sanity.js
+++ sanity.js
@@ -1,14 +1,11 @@
-import {
-    // createImageUrlBuilder,
-    // createCurrentUserHook,
-    createClient
-} from "next-sanity"
+import { createClient } from "next-sanity"
 import createImageUrlBuilder from '@sanity/image-url'
 
 export const config = {
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
-    dataset: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-    useCdn: process.env.NODE.ENV === "production",
+    apiVersion: '2021-08-31', // use a UTC date string
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    useCdn: process.env.NODE_ENV === "production",
 }
 
 export const sanityClient = createClient(config)
